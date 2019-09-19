f8f16db018f8bb022d10a21262175de014dc67ad
Stefan Rosu <rosus89@gmail.com>
2019-09-19 17:05:12 +0100

    Style: replaced table

--- src/components/transactions.js
+++ src/components/transactions.js
@@ -1,41 +1,74 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles';
-import { Table, TableBody, TableCell, TableHead, TableRow, Paper, Typography, Button } from '@material-ui/core';
+import clsx from 'clsx';
+import { Box,
+         Paper, 
+         Typography, 
+         Button,
+         ExpansionPanel,
+         ExpansionPanelSummary,
+         ExpansionPanelDetails
+        } from '@material-ui/core';
 
 const styles = makeStyles(theme => ({
     transactions:{
         minHeight: '300px',
         margin: '4px 0'
+    },
+
+    date: {
+        margin: "auto",
+        paddingLeft: "0.5em"
+    },
+
+    flex25: {
+        flexBasis: '25%',
+        padding: "0 0.5em"
+    },
+
+    flex50: {
+        flexBasis: '50%'
+    },
+
+    expansionDetails:{
+        background: '#d4d2d2',
+        padding: 0
     }
 }));
 
 function Transactions(props){
     let classes = styles();
     let output = <Typography>There are no transactions</Typography>;
-
+    console.log(props.transactions)
     if (props.transactions.length > 0) {
-        output = (<Table className={classes.table}>
-            <TableHead>
-                <TableRow>
-                    <TableCell size="small">Currency</TableCell>
-                    <TableCell size="small">Amount</TableCell>
-                    <TableCell size="small">Price (unit)</TableCell>
-                    <TableCell size="small">Total Cost</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
+        output = (
+        <div>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography className={classes.flex25}>Currency</Typography>
+                    <Typography className={classes.flex25}>Amount</Typography>
+                    <Typography className={classes.flex25}>Price (unit)</Typography>
+                    <Typography className={classes.flex25}>Total Cost</Typography>
+                </Box>
+            <div>
                 {props.transactions.map((transaction, index) => (
-                    <TableRow key={index}>
-                        <TableCell size="small">{transaction.from}/{transaction.to}</TableCell>
-                        <TableCell size="small">{transaction.amount} {transaction.to}</TableCell>
-                        <TableCell size="small">{transaction.price} {transaction.from}</TableCell>
-                        <TableCell size="small">{transaction.amount * transaction.price} {transaction.from}</TableCell>
-                        <TableCell size="small"><Button onClick={()=>{props.deleteTransaction(transaction, index)}}>Delete</Button></TableCell>
-                    </TableRow>
+                    <ExpansionPanel key={index}>
+                        <ExpansionPanelSummary>
+                            <Box width={1} display="flex" justifyContent="space-between">
+                                <Typography className={classes.flex25}>{transaction.from}/{transaction.to}</Typography>
+                                <Typography className={classes.flex25}>{transaction.amount} {transaction.to}</Typography>
+                                <Typography className={classes.flex25}>{transaction.price} {transaction.from}</Typography>
+                                <Typography className={classes.flex25}>{transaction.amount * transaction.price} {transaction.from}</Typography>
+                            </Box>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails className={classes.expansionDetails}>
+                            <Typography className={clsx(classes.flex50, classes.date)}>{transaction.date}</Typography>
+                            <Button className={classes.flex50} onClick={() => { props.deleteTransaction(transaction, index) }}>Delete</Button>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
                 ))}
-            </TableBody>
-        </Table>)
+            </div>
+        </div>)
     }
     return (
         <Paper className={classes.transactions}>{output}</Paper>
